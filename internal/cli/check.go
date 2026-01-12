9ac3a9be5e74ccb167f903eba3f3e078ce24c219
aidanlsb <37315016+aidanlsb@users.noreply.github.com>
2026-01-11 18:42:47 -0800

    Revert "fix(check): respect directory organization when parsing object IDs"
    This reverts commit 588bcb43c85de368a013ca081f73213eda1eafaa.

--- internal/cli/check.go
+++ internal/cli/check.go
@@ -13,7 +13,6 @@ import (
 	"gopkg.in/yaml.v3"
 
 	"github.com/aidanlsb/raven/internal/check"
-	"github.com/aidanlsb/raven/internal/config"
 	"github.com/aidanlsb/raven/internal/index"
 	"github.com/aidanlsb/raven/internal/pages"
 	"github.com/aidanlsb/raven/internal/parser"
@@ -307,20 +306,6 @@ var checkCmd = &cobra.Command{
 			}
 		}
 
-		// Load vault config for directory roots (used to strip prefixes from object IDs)
-		vaultCfg, _ := config.LoadVaultConfig(vaultPath)
-
-		// Build parse options from vault config
-		var walkOpts *vault.WalkOptions
-		if vaultCfg.HasDirectoriesConfig() {
-			walkOpts = &vault.WalkOptions{
-				ParseOptions: &parser.ParseOptions{
-					ObjectsRoot: vaultCfg.GetObjectsRoot(),
-					PagesRoot:   vaultCfg.GetPagesRoot(),
-				},
-			}
-		}
-
 		// Determine which files to walk based on scope
 		var walkPath string
 		var targetFileSet map[string]bool
@@ -342,7 +327,7 @@ var checkCmd = &cobra.Command{
 
 		// First pass: parse all documents in the vault to collect object IDs
 		// (needed for reference validation even when checking a subset)
-		err = vault.WalkMarkdownFilesWithOptions(vaultPath, walkOpts, func(result vault.WalkResult) error {
+		err = vault.WalkMarkdownFiles(vaultPath, func(result vault.WalkResult) error {
 			if result.Error != nil {
 				// Only count files in scope
 				if isFileInScope(result.Path, scope, walkPath, targetFileSet) {
